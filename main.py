5e3b71258d984687cd43e97efea22b48bbf17d17
UARTman <agus179e@gmail.com>
2019-04-18 20:33:28 +0300

    Сделан простенький интерфейс - поле ввода имени файла и кнопка

--- main.py
+++ main.py
@@ -2,22 +2,10 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from Engine import parse_be, clean, funcparse, ScrollBlock
+from Interface import MainWindow
 
 if __name__ == '__main__':
+    mainApp = QApplication(sys.argv)
+    a = MainWindow()
+    sys.exit(mainApp.exec_())
 
-    with open('in.pas', 'r', encoding='utf-8') as f:
-        InputStr = ' ' + f.read()
-    OutputArray = []
-
-    try:
-        parse_be(start=0, out=OutputArray, inp=InputStr)
-    except TypeError:
-        pass
-
-    OutputArray = clean(OutputArray)
-    OutputArray = funcparse(OutputArray)
-    OutputArray = clean(OutputArray)
-    app = QApplication(sys.argv)
-    bl = ScrollBlock(inp=['block', OutputArray])
-    sys.exit(app.exec_())
